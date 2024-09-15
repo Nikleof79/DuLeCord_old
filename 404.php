832383ad6ac8cb1ca8a91be4af45610bdd4caca0
Nikleof79 <165087676+Nikleof79@users.noreply.github.com>
2024-09-15 22:26:46 +0200

    no router 😭

---
+++ 404.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DuLeCord</title>
+    <?php 
+    include 'assets/inc/links.php'
+    ?>
+    <style>
+        body{
+            background-color: darkblue;
+            color: white;
+            display: flex;
+            justify-content: center;
+            min-width: 100vw;
+            height: 100vh;
+            overflow: hidden;
+        }
+        div{
+            text-align: center;
+            max-width: 520px;
+        }
+        img{
+            width: 100%;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <img src='./assets/img/logo.png' alt="">
+        <h1>404</h1>
+        <h1>This page not found</h1>
+        <a href="/" class="btn">Go to index</a>
+    </div>
+</body>
+</html>
