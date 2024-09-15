3df333441d322a4ced678681841e8897e5a0374e
Nikleof79 <165087676+Nikleof79@users.noreply.github.com>
2024-09-15 22:00:31 +0200

    rouetr

--- router.php
+++ router.php
@@ -1,33 +1,10 @@
 <?php
-include '/library/bulbaPHP.php';
-
-$app = new BulbaApp();
-
-function oneOf($array,$target){
-    $ret_data = false;
-    foreach ($array as $kay => $value) {
-        if ($value == $target) {
-            $ret_data = true;
-            break;
-        }
+class Router{
+    private $static_folder;
+    private $handler_folder;
+    function __construct() {
+        $this->static_folder = './assets';
+        $this->handler_folder = './handlers';
     }
-    return $ret_data;
+    
 }
-
-$app->use(['/'],'',function($req,$res){
-    $req->redirect('/index.php');
-});
-
-$app->use([],'*',function($req,$res){
-    $pages = ['account','friends','index','start'];
-    $url_splited = explode('/', $req->url);
-    if ($url_splited[0] == '/assets') {
-        $res->sendFile($req->url);
-    }else if($url_splited[0] == '/handlers'){
-        $res->sendFile($req->url);
-    }else if(oneOf($pages , $req->url)){
-        $res->sendFile($req->url + '.php');
-    }else if(oneOf($pages , $req->url + '.php')){
-        $res->sendFile($req->url);
-    }
-});
