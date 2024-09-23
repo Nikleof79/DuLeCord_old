3adc5ee927f88c7ee90e919832a3bc02354115d1
Nikleof79 <165087676+Nikleof79@users.noreply.github.com>
2024-09-23 20:09:39 +0200

    Update send_message.php

--- handlers/api/send_message.php
+++ handlers/api/send_message.php
@@ -1,18 +1,21 @@
-<?php 
+<?php
+session_start();
 include '../../assets/inc/mysql.php';
 
-
-function checks($request) {
+function checks($request)
+{
     $ret_data = false;
     if (isset($request)) {
-        
+        if ($_SESSION['logined']) {
+
             $mysql = new BulbaSqlConn('../../security/passsql.json');
             $friends = $mysql->query("
                 SELECT * FROM friends WHERE 
                 requester = ' " . $_SESSION['login-data']['username'] . "'
-                AND reciver = '". $request['reciever']."'
-            ;")->fetch_assoc(); 
+                AND reciver = '" . $request['reciever'] . "'
+            ;")->fetch_assoc();
             $ret_data = !(!$friends); // conversation to bool
+        }
     }
     return $ret_data;
 }
@@ -21,6 +24,7 @@ $checks = checks($_POST);
 if ($checks) {
     $MySql = new BulbaSqlConn('../../security/passsql.json');
     $MySql->query("
-        INSERT INTO messages () VALUES 
+        INSERT INTO messages (body, requester, reciever) 
+        VALUES ('" . $_POST['body'] . "' '" . $_SESSION['login-data']['username'] . "' , ' " . $_POST['reciever'] . "')
     ;");
 }
