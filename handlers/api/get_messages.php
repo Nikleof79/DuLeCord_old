7be1564d35c24939d391f33d27779334c2ca069d
Nikleof79 <165087676+Nikleof79@users.noreply.github.com>
2024-10-27 09:56:18 +0100

    no sound😭

--- handlers/api/get_messages.php
+++ handlers/api/get_messages.php
@@ -6,7 +6,7 @@ include '../../assets/inc/mysql.php';
 function checks($req){
     $ret_data = false;
     if (isset($req) && isset($req['intercultor'])) {
-        if ($_SESSION['logined']) {
+        if ($_SESSION['logined'] && $_SERVER['REQUEST_METHOD'] == "POST") {
             $mysql = new BulbaSqlConn('../../security/passsql.json');
             $sql = "
             SELECT reciver FROM friends WHERE requester = '" . $_SESSION['login-data']['username'] . "'
@@ -21,6 +21,9 @@ function checks($req){
 }
 $ret_data = null;
 $checks = checks($_POST);
+// $checks = true;
+// $_SESSION['login-data']['username'] = 'nikman78';
+// $_POST['intercultor'] = 'copilot';
 if ($checks) {
     $MySql = new BulbaSqlConn('../../security/passsql.json');
     $sql = "
@@ -28,11 +31,9 @@ if ($checks) {
         OR (reciever = '" . $_SESSION['login-data']['username'] ."' AND requester = '" . $_POST['intercultor'] . "') ORDER BY timestamp
     ;";
     $messages = $MySql->query($sql);
-    $ret_data = [
-        'messages' => $messages->fetch_all()
-    ];
+    $ret_data = $messages->fetch_all(MYSQLI_ASSOC);
 }
 
 header('Content-type: json');
 echo json_encode($ret_data);
-exit;
+exit;   
